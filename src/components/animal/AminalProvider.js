10d6fe357d6639feb8c96485bdcbf5dd927a700c
john gallagher <127highstreet@gmail.com>
2021-12-06 08:25:00 -0500

    ch5 instructions

---
+++ src/components/animal/AminalProvider.js
@@ -0,0 +1,46 @@
+// In the React library, there is a feature called the Context API. This API provides you with two critical functions.
+
+// createContext() - Create the context to be used by other components that need data.
+// useContext() - Used by UI components that need data stored in the context, and exposed by the provider component.
+
+
+import React, { useState, createContext } from "react"
+
+// The context is imported and used by individual components that need data
+export const AnimalContext = createContext()
+
+// This component establishes what data can be used.
+export const AnimalProvider = (props) => {
+    const [animals, setAnimals] = useState([])
+
+    const getAnimals = () => {
+        return fetch("http://localhost:8088/animals?_expand=location")
+        .then(res => res.json())
+        .then(setAnimals)
+    }
+
+    const addAnimal = animalObj => {
+        return fetch("http://localhost:8088/animals", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(animalObj)
+        })
+        .then(getAnimals)
+    }
+
+    /*
+        You return a context provider which has the
+        `animals` state, `getAnimals` function,
+        and the `addAnimal` function as keys. This
+        allows any child elements to access them.
+    */
+    return (
+        <AnimalContext.Provider value={{
+            animals, getAnimals, addAnimal
+        }}>
+            {props.children}
+        </AnimalContext.Provider>
+    )
+}
