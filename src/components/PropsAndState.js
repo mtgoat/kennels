daff18e7d473812edaa572fa05936770aea6e664
john gallagher <127highstreet@gmail.com>
2021-12-05 20:03:00 -0500

    saving for the props

--- src/components/PropsAndState.js
+++ src/components/PropsAndState.js
@@ -1,7 +1,7 @@
 //props = properties, Conceptually, components are like JavaScript functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen. props name and 
 
 import React from "react"
-
+import ReactDOM from 'react-dom';
 export const PropsAndState = ({ yourName }) => {
 
   return (
@@ -10,3 +10,14 @@ export const PropsAndState = ({ yourName }) => {
     </>
   )
 }
+
+// {/*<PropsAndState yourName={"Aki"}/> in Kennel.js. Aki is the prop   and the parent component is <PropAndState yourName={"Aki"}> and the child component is in PropsAndState.js //
+
+//this is the same as the PropsAndState where the ReactDOM emement is a parent and fuction is a child component//
+ function Welcome (props) {
+  return <h3>Hello {props.name} </h3>
+}
+
+const element = <Welcome name="Aki"/>
+ReactDOM.render(
+  element, document.getElementById('rootTwo'))
